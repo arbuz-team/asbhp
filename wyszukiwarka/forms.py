39ef1405a170c0d110ecc92f8e6970f8e5991d3d
arbuz93 <93.endo@gmail.com>
2016-06-13 02:15:03 +0200

    Dodanie klasy 'focus' dla formularzy filtrów.

--- wyszukiwarka/forms.py
+++ wyszukiwarka/forms.py
@@ -4,8 +4,11 @@ from produkt.models import *
 
 class Formularz_Wyszukiwarki(forms.Form):
 
-    zapytanie = forms.CharField(label='', error_messages=
-        {'required': ''})
+    zapytanie = forms.CharField(
+        label='',
+        error_messages={'required': ''},
+        widget=forms.TextInput(attrs={'class': 'focus'})
+    )
 
     def clean_zapytanie(self):
         zapytanie = self.cleaned_data['zapytanie']
@@ -23,6 +26,7 @@ class Formularz_Filtru_Producent(forms.ModelForm):
     class Meta:
         model = Produkt
         fields = ('producent',)
+        widgets = {'producent': forms.Select(attrs={'class': 'focus'})}
 
 
 class Formularz_Filtru_Kolor(forms.ModelForm):
@@ -30,6 +34,7 @@ class Formularz_Filtru_Kolor(forms.ModelForm):
     class Meta:
         model = Produkt
         fields = ('kolor',)
+        widgets = {'kolor': forms.Select(attrs={'class': 'focus'})}
 
 
 class Formularz_Filtru_Zagrozenia(forms.ModelForm):
@@ -37,6 +42,7 @@ class Formularz_Filtru_Zagrozenia(forms.ModelForm):
     class Meta:
         model = Produkt
         fields = ('zagrozenia',)
+        widgets = {'zagrozenia': forms.SelectMultiple(attrs={'class': 'focus'})}
 
 
 class Formularz_Filtru_Zawody(forms.ModelForm):
@@ -44,16 +50,20 @@ class Formularz_Filtru_Zawody(forms.ModelForm):
     class Meta:
         model = Produkt
         fields = ('zawody',)
+        widgets = {'zawody': forms.SelectMultiple(attrs={'class': 'focus'})}
 
 
 class Formularz_Filtru_Liczba_Produktow(forms.Form):
-    liczba = forms.ChoiceField(choices=(
-        (4, 4),
-        (8, 8),
-        (12, 12),
-        (16, 16),
-        (20, 20),
-    ))
+    liczba = forms.ChoiceField(
+        choices=
+        (
+            (4, 4),
+            (8, 8),
+            (12, 12),
+            (16, 16),
+            (20, 20),
+        ),
+        widget=forms.Select(attrs={'class': 'focus'}))
 
 
 class Formularz_Kontener(forms.Form):
