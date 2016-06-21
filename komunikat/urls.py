ea9a288ad4be0462ccc8c887d7294485c27a624d
arbuz93 <93.endo@gmail.com>
2016-06-21 14:53:45 +0200

    Dodałem komunikat wysłania maila razem z przekierowaniami do niego po wysłaniu maili z formularzy.

--- komunikat/urls.py
+++ komunikat/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
 
     url(r'^404/$', views.Wyswietl_404, name='Wyswietl_404'),
+    url(r'^email_wyslany/$', views.Wyswietl_Email_Wyslany,
+        name='Wyswietl_Email_Wyslany'),
 
 ]
