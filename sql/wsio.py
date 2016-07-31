e367d411290c50068078a89fbcac05967568d614
arbuz93 <93.endo@gmail.com>
2016-07-31 17:40:26 +0200

    Wpisałem na nowo wszystkie produkty - teraz jest na czysto.

--- sql/wsio.py
+++ sql/wsio.py
@@ -1,4 +1,8 @@
 import sql.asbhp_db
 import sql.produkt_db
 import sql.uzytkownik_db
-#import sql.deltaplus
+import sql.deltaplus
+import sql.industrialstarter
+import sql.adler
+import sql.filterservice
+import sql.protekt
